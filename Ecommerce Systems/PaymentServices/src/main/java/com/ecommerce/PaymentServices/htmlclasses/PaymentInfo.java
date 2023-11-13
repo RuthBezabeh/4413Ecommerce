ce522774c93bc519d342380ca218c4a4755adb12
Ruth Bezabeh <ruttkas@my.yorku.ca>
2023-11-13 17:30:04 -0500

    update expire date datatype and add paymentInfo class

---
+++ Ecommerce Systems/PaymentServices/src/main/java/com/ecommerce/PaymentServices/htmlclasses/PaymentInfo.java
@@ -0,0 +1,10 @@
+package com.ecommerce.PaymentServices.htmlclasses;
+
+import java.util.Date;
+public class PaymentInfo {
+    private String card_fname;
+    private String card_lname;
+    private String card_number;
+    private Date card_expire_date;
+    private int security_code;
+}
